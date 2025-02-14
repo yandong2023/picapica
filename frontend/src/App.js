95b3a86ea5d457dcbbfcf9175c184d20c0daeed5
Agneswei <agnesye153@gmail.com>
2025-02-13 22:54:00 -0500

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PhotoBooth from "./PhotoBooth"; // Import your photo booth component
+import "./App.css"; // Component styles
+
+function App() {
+  return (
+    <div className="app">
+      <h1>PhotoBooth App 📸</h1>
+      <PhotoBooth />
+    </div>
+  );
+}
+
+export default App;
