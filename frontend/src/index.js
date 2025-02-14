39d56b6894e0f6a927a4f993c646ac2d28531a86
Agneswei <agnesye153@gmail.com>
2025-02-13 22:54:06 -0500

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";  // Global styles
+import App from "./App"; // Main component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
