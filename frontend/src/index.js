d47b355ec057a741b4285890573197734abb78fb
Agnes <agnesye153@gmail.com>
2025-02-17 17:59:00 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename="/">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
