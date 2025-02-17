2041f7888260faf5e6057ef59959cfc392489de4
Agnes <agnesye153@gmail.com>
2025-02-17 15:58:05 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import "./index.css";  
 import App from "./App"; 
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Router basename="./picapica">
+    <Router basename="/picapica">
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
