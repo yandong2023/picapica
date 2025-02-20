326e8ebcb92d1e0e10c2a9a0b7baa088276e3929
Agnes <agnesye153@gmail.com>
2025-02-19 22:01:52 -0500

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,27 +1,34 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "../App.css";
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="home-container">
-      <h1>picapica</h1>
-      <p>Welcome to Agnes' photobooth! This is your personal photobooth at home.</p>
-      
-      <img src="/photobooth-strip.png" alt="photobooth strip" className="photobooth-strip"/>
-      
-      <button onClick={() => navigate("/welcome")}>START</button>
-      <footer>
-        made by{" "}
-        <a
-          href="https://agneswei.com" 
-          target="_blank" 
-          rel="noopener noreferrer" 
-          style={{ color: "pink", textDecoration: "none" }}>agneswei</a>
-      </footer>
+    <div className="background-gradient h-screen flex  flex-col justify-center items-center text-center">
+      <div className="home-container">
+        <h1 className="text-5xl font-bold text-pink-600 mb-4">picapica</h1>
+        <p className="text-lg text-gray-700 mb-6">
+          Welcome to Agnes' photobooth! This is your personal photobooth at home.
+        </p>      
+          
+        <img src="/photobooth-strip.png" alt="photobooth strip" className="photobooth-strip"/>
+        
+        <button onClick={() => navigate("/welcome")} className="bg-pink-500 text-white px-6 py-3 rounded-lg hover:bg-pink-600 transition"
+        >START</button>
+
+        <footer className="mt-8 text-sm text-gray-600">
+          made by{" "}
+          <a
+            href="https://agneswei.com" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            style={{ color: "pink", textDecoration: "none" }}>agneswei</a>
+        </footer>
+      </div>
     </div>
-  );
-};
+    );
+  };
 
 export default Home;
