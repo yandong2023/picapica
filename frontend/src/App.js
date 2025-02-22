fd964b0249f28382de2a064418e7b7f40d088665
Agnes <agnesye153@gmail.com>
2025-02-22 15:32:26 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,31 @@
 import "./App.css";
 import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";  
+import { Routes, Route, Link } from "react-router-dom";  
 import Home from "./components/Home";
 import Welcome from "./components/Welcome";
 import PhotoBooth from "./components/PhotoBooth";
 import PhotoPreview from "./components/PhotoPreview";
+import PrivacyPolicy from './components/PrivacyPolicy';
+
 
 function App() {
   const [capturedImages, setCapturedImages] = useState([]);
 
   return (
+    <div className="App">
+      <nav className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/privacy-policy">Privacy Policy</Link>
+      </nav>
+  
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/welcome" element={<Welcome />} />
         <Route path="/photobooth" element={<PhotoBooth setCapturedImages={setCapturedImages} />} />
         <Route path="/preview" element={<PhotoPreview capturedImages={capturedImages} />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
+    </div>
   );
 }
 
